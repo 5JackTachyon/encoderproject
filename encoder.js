9b5a766ac326138f4ba13dd0ce0abadd7a83191b
Sean Dow Miller <seandowmiller@gmail.com>
2016-10-20 19:19:45 -0700

    yeah

--- encoder.js
+++ encoder.js
@@ -414,127 +414,128 @@ function keyCodeAll(event) {
 
     var x = event.keyCode;
     if (x == 65) {
-      document.getElementById("translationArea").innerHTML += "a";
-    }
-    else if(x == 66) {
       document.getElementById("translationArea").innerHTML += "b";
     }
-    else if(x == 67) {
+    else if(x == 66) {
       document.getElementById("translationArea").innerHTML += "c";
     }
-    else if(x == 68) {
+    else if(x == 67) {
       document.getElementById("translationArea").innerHTML += "d";
     }
-    else if(x == 69) {
+    else if(x == 68) {
       document.getElementById("translationArea").innerHTML += "e";
     }
-    else if(x == 70) {
+    else if(x == 69) {
       document.getElementById("translationArea").innerHTML += "f";
     }
-    else if(x == 71) {
+    else if(x == 70) {
       document.getElementById("translationArea").innerHTML += "g";
     }
-    else if(x == 72) {
+    else if(x == 71) {
       document.getElementById("translationArea").innerHTML += "h";
     }
-    else if(x == 73) {
+    else if(x == 72) {
       document.getElementById("translationArea").innerHTML += "i";
     }
-    else if(x == 74) {
+    else if(x == 73) {
       document.getElementById("translationArea").innerHTML += "j";
     }
-    else if(x == 75) {
+    else if(x == 74) {
       document.getElementById("translationArea").innerHTML += "k";
     }
-    else if(x == 76) {
+    else if(x == 75) {
       document.getElementById("translationArea").innerHTML += "l";
     }
-    else if(x == 77) {
+    else if(x == 76) {
       document.getElementById("translationArea").innerHTML += "m";
     }
-    else if(x == 78) {
+    else if(x == 77) {
       document.getElementById("translationArea").innerHTML += "n";
     }
-    else if(x == 79) {
+    else if(x == 78) {
       document.getElementById("translationArea").innerHTML += "o";
     }
-    else if(x == 80) {
+    else if(x == 79) {
       document.getElementById("translationArea").innerHTML += "p";
     }
-    else if(x == 81) {
+    else if(x == 80) {
       document.getElementById("translationArea").innerHTML += "q";
     }
-    else if(x == 82) {
+    else if(x == 81) {
       document.getElementById("translationArea").innerHTML += "r";
     }
-    else if(x == 83) {
+    else if(x == 82) {
       document.getElementById("translationArea").innerHTML += "s";
     }
-    else if(x == 84) {
+    else if(x == 83) {
       document.getElementById("translationArea").innerHTML += "t";
     }
-    else if(x == 85) {
+    else if(x == 84) {
       document.getElementById("translationArea").innerHTML += "u";
     }
-    else if(x == 86) {
+    else if(x == 85) {
       document.getElementById("translationArea").innerHTML += "v";
     }
-    else if(x == 87) {
+    else if(x == 86) {
       document.getElementById("translationArea").innerHTML += "w";
     }
-    else if(x == 88) {
+    else if(x == 87) {
       document.getElementById("translationArea").innerHTML += "x";
     }
-    else if(x == 89) {
+    else if(x == 88) {
       document.getElementById("translationArea").innerHTML += "y";
     }
-    else if(x == 90) {
+    else if(x == 89) {
       document.getElementById("translationArea").innerHTML += "z";
     }
-    else if(x == 32) {
-      document.getElementById("translationArea").innerHTML += " ";
+    else if(x == 90) {
+      document.getElementById("translationArea").innerHTML += "a";
     }
-    else if(x == 49) {
+    else if(x == 32) {
+      /*I think this is the only messed up letter in the decoder*/
       document.getElementById("translationArea").innerHTML += "1";
     }
-    else if(x == 191) {
+    else if(x == 49) {
       document.getElementById("translationArea").innerHTML += "?";
     }
-    else if(x == 188) {
+    else if(x == 191) {
       document.getElementById("translationArea").innerHTML += ",";
     }
-    else if(x == 190) {
+    else if(x == 188) {
       document.getElementById("translationArea").innerHTML += ".";
     }
-    else if(x == 48) {
+    else if(x == 190) {
       document.getElementById("translationArea").innerHTML += "0";
     }
-    else if(x == 50) {
+    else if(x == 48) {
       document.getElementById("translationArea").innerHTML += "2";
     }
-    else if(x == 51) {
+    else if(x == 50) {
       document.getElementById("translationArea").innerHTML += "3";
     }
-    else if(x == 52) {
+    else if(x == 51) {
       document.getElementById("translationArea").innerHTML += "4";
     }
-    else if(x == 53) {
+    else if(x == 52) {
       document.getElementById("translationArea").innerHTML += "5";
     }
-    else if(x == 54) {
+    else if(x == 53) {
       document.getElementById("translationArea").innerHTML += "6";
     }
-    else if(x == 55) {
+    else if(x == 54) {
       document.getElementById("translationArea").innerHTML += "7";
     }
-    else if(x == 56) {
+    else if(x == 55) {
       document.getElementById("translationArea").innerHTML += "8";
     }
-    else if(x == 57) {
+    else if(x == 56) {
       document.getElementById("translationArea").innerHTML += "9";
     }
-    else if(x == 187) {
+    else if(x == 57) {
       document.getElementById("translationArea").innerHTML += "=";
+    }
+    else if(x == 187) {
+      document.getElementById("translationArea").innerHTML += " ";
     }
 
 
